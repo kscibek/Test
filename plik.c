345c1411e2b65468ee3ef9f3c81907aa5f6d2ddc
Katarzyna Ścibek <kscibek@p110-10.labpk.inf.ug.edu.pl>
2012-11-27 12:51:57 +0100

    Committer: Katarzyna Ścibek <kscibek@p110-10.labpk.inf.ug.edu.pl>

---
+++ plik.c
@@ -0,0 +1,29 @@
+
+Środowisko programisty
+Ścieżka do strony
+
+    Moja strona domowa
+    / ► Moje kursy
+    / ► NK80766098
+    / ► Temat 7
+    / ► Git
+
+Git
+
+Przeprowadzić następujące operacje,
+samemu:
+
+    założyć sobie konto na http://github.com/
+    założyć testowe repozytorium, (zaznaczyć checkbox "Initialize with a README file"),
+    ściągnąć kopię repozytorium na sigmie, (clone)
+    ściągnąć w wersji możliwej do edycji, nie read-only,
+    wiedzieć jaka jest różnica :-)
+    stworzyć nowy niepusty plik, dodać do repozytorium (lokalnej kopii) (add, commit)
+    wyeksportować na githuba (push),
+    sprawdzić przez www czy plik się pojawił,
+    ściągnąć repozytorium do drugiego, nowego katalogu,
+    w drugim repozytorium zmienić coś w utworzonym poprzednio pliku,
+    zatwierdzić zmianę i wrzucić na githuba,
+    ściągnąć nową wersję w pierwszej kopii repozytorium (pull)
+
+We współpracy z innymi (zespoły 2-3 osobowe),
